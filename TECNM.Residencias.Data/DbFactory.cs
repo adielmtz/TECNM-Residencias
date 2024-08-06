17a2c2209b0118b91181eb35aebd71b7ca3121d8
Luis Adiel Martínez <luisadiel99@gmail.com>
2024-08-06 01:33:01 -0600

    Add DbFactory class

---
+++ TECNM.Residencias.Data/DbFactory.cs
@@ -0,0 +1,53 @@
+using Microsoft.Data.Sqlite;
+using System.Data;
+using System.Diagnostics;
+
+namespace TECNM.Residencias.Data
+{
+    public sealed class DbFactory
+    {
+        private readonly string _dataSource;
+        private string? _connectionString;
+
+        public string DataSource => _dataSource;
+
+        public string ConnectionString
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_connectionString))
+                {
+                    _connectionString = new SqliteConnectionStringBuilder
+                    {
+                        DataSource = DataSource,
+                        ForeignKeys = true,
+                        RecursiveTriggers = true,
+                    }.ConnectionString;
+                }
+
+                return _connectionString;
+            }
+        }
+
+        public DbFactory(string dataSource)
+        {
+            _dataSource = dataSource;
+        }
+
+        public SqliteConnection Open()
+        {
+            var sqlite = new SqliteConnection(ConnectionString);
+            sqlite.Open();
+            ConfigureDatabase(sqlite);
+            return sqlite;
+        }
+
+        private void ConfigureDatabase(SqliteConnection sqlite)
+        {
+            Debug.Assert(sqlite.State == ConnectionState.Open);
+            using var command = sqlite.CreateCommand();
+            command.CommandText = "PRAGMA journal_mode=wal;";
+            command.ExecuteNonQuery();
+        }
+    }
+}
