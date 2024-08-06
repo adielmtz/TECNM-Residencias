b3ba4103b8bcb78e56261c5fdcaee8224500a37e
Luis Adiel Martínez <luisadiel99@gmail.com>
2024-08-06 01:31:58 -0600

    Initialize VS project

---
+++ TECNM.Residencias/Forms/MainWindow.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace TECNM.Residencias.Forms
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // MainWindow
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(1264, 681);
+            Name = "MainWindow";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "MainWindow";
+            WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            Load += MainWindow_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ TECNM.Residencias/Forms/MainWindow.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Windows.Forms;
+
+namespace TECNM.Residencias.Forms
+{
+    public sealed partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void ShowCareers_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void ShowCompanies_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
