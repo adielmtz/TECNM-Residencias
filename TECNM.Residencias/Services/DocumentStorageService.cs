56a668e07af310dd2cc880a8ddcbcc6cbbb2ee63
Luis Adiel Martínez <luisadiel99@gmail.com>
2024-09-04 15:44:16 -0600

    Return type index for document filename to prevent accidental overwrite when updating the document type in the combobox

--- TECNM.Residencias/Services/DocumentStorageService.cs
+++ TECNM.Residencias/Services/DocumentStorageService.cs
@@ -14,7 +14,7 @@ namespace TECNM.Residencias.Services
             string yearComponent = student.StartDate.Year.ToString();
             string semesterComponent = student.Semester;
             string extension = Path.GetExtension(filename);
-            string targetName = $"{student.Id}_{typeName}_{document.Hash.Substring(0, 8)}{extension}";
+            string targetName = $"{student.Id}_{document.Type}_{typeName.ToUpper()}_{document.Hash.Substring(0, 8)}{extension}";
 
             string targetFile = Path.Combine(App.DocumentArchiveDirectory, yearComponent, semesterComponent, $"{student.Id}", targetName);
             string directory = Path.GetDirectoryName(targetFile)!;
@@ -24,7 +24,7 @@ namespace TECNM.Residencias.Services
                 Directory.CreateDirectory(directory);
             }
 
-            File.Copy(filename, targetFile);
+            File.Copy(filename, targetFile, true);
             document.FullPath = targetFile;
         }
     }
