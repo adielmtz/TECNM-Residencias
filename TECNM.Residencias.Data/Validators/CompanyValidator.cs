40fdeb37021129518daf7cfa51191b430b91bb98
Luis Adiel Martínez <luisadiel99@gmail.com>
2024-09-17 23:54:23 -0600

    Add RFC validation. Closes #3

--- TECNM.Residencias.Data/Validators/CompanyValidator.cs
+++ TECNM.Residencias.Data/Validators/CompanyValidator.cs
@@ -9,7 +9,12 @@ namespace TECNM.Residencias.Data.Validators
         public CompanyValidator()
         {
             ClassLevelCascadeMode = CascadeMode.Stop;
-            RuleFor(c => c.Rfc).NotEmpty().When(c => c.Rfc != null).WithMessage("Debe proporcionar el RFC de la empresa.");
+            RuleFor(c => c.Rfc)
+                .Length(12, 13)
+                .Matches(@"^[A-Z&Ñ]{3,4}[0-9]{2}(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[01])[A-Z0-9]{2}[0-9A]$")
+                .When(c => c.Rfc != null)
+                .WithMessage("Ingrese un RFC válido.");
+
             RuleFor(c => c.Type).IsInEnum().WithMessage("Debe seleccionar el giro de la empresa.");
             RuleFor(c => c.Name).NotEmpty().WithMessage("El nombre de la empresa no puede estar vacío.");
             RuleFor(c => c.Email).EmailAddress().When(c => c.Email.Length > 0).WithMessage("Debe introducir una dirección de email válida.");
