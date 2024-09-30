747e9323c40246ac546708771fdb3d5248e736ae
Luis Adiel Martínez <luisadiel99@gmail.com>
2024-09-30 13:23:40 -0600

    Add required indexes to the database migration schema

--- TECNM.Residencias.Data.Migrations/Resources/migration_2.sql
+++ TECNM.Residencias.Data.Migrations/Resources/migration_2.sql
@@ -1,1 +1,52 @@
-PRAGMA optimize;
+CREATE INDEX IX_Company_SortName ON Company (
+    Name
+);
+
+CREATE INDEX IX_Company_CityId ON Company (
+    CityId
+);
+
+
+CREATE INDEX IX_Advisor_Lookup ON Advisor (
+    CompanyId,
+    FirstName,
+    LastName
+);
+
+
+CREATE INDEX IX_Student_SpecialtyId ON Student (
+    SpecialtyId
+);
+
+
+CREATE INDEX IX_Student_InternalAdvisorId ON Student (
+    InternalAdvisorId
+);
+
+
+CREATE INDEX IX_Student_ExternalAdvisorId ON Student (
+    ExternalAdvisorId
+);
+
+
+CREATE INDEX IX_Student_ReviewerAdvisorId ON Student (
+    ReviewerAdvisorId
+);
+
+
+CREATE INDEX IX_Student_CompanyId ON Student (
+    CompanyId
+);
+
+
+CREATE INDEX IX_Student_LastUpdated ON Student (
+    UpdatedOn
+);
+
+
+CREATE INDEX IX_Student_BySemester_Expr ON Student (
+    strftime('%Y', StartDate),
+    Semester,
+    FirstName,
+    LastName
+);
