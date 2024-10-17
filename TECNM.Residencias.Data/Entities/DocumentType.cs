b5b210437a1178795882e64a69a5029fa59bb515
Luis Adiel Martínez <luisadiel99@gmail.com>
2024-10-16 18:14:31 -0600

    Add code documentation to entity types

--- TECNM.Residencias.Data/Entities/DocumentType.cs
+++ TECNM.Residencias.Data/Entities/DocumentType.cs
@@ -1,15 +1,34 @@
 namespace TECNM.Residencias.Data.Entities;
 
+/// <summary>
+/// Represents the type of a document entity in the database.
+/// </summary>
 public sealed class DocumentType
 {
+    /// <summary>
+    /// Gets or sets the unique rowid of the entity.
+    /// </summary>
     public long Id { get; set; }
 
+    /// <summary>
+    /// Gets or sets the document type label.
+    /// </summary>
     public string Label { get; set; } = "";
 
+    /// <summary>
+    /// Gets or sets the file tag.
+    /// </summary>
     public string Tag { get; set; } = "";
 
+    /// <summary>
+    /// Gets or sets the document type keywords.
+    /// </summary>
     public string Keywords { get; set; } = "";
 
+    /// <summary>
+    /// Returns the document type label as string representation.
+    /// </summary>
+    /// <returns>The label of the document type.</returns>
     public override string ToString()
     {
         return Label;
--- TECNM.Residencias.Data/Entities/Gender.cs
+++ TECNM.Residencias.Data/Entities/Gender.cs
@@ -1,11 +1,24 @@
 namespace TECNM.Residencias.Data.Entities;
 
+/// <summary>
+/// Represents a gender of an student entity in the database.
+/// </summary>
 public sealed class Gender
 {
+    /// <summary>
+    /// Gets or sets the unique rowid of the entity.
+    /// </summary>
     public long Id { get; set; }
 
+    /// <summary>
+    /// Gets or sets the gender label.
+    /// </summary>
     public string Label { get; set; } = "";
 
+    /// <summary>
+    /// Returns the gender label as string representation.
+    /// </summary>
+    /// <returns>The label of the gender.</returns>
     public override string ToString()
     {
         return Label;
