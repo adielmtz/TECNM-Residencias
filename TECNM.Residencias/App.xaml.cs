ad00a70be4946d53ab564744b1dc1a3f6b85954f
Luis Adiel Martínez <luisadiel99@gmail.com>
2024-10-22 20:11:38 -0600

    Replace UI project from WinForms to WPF

---
+++ TECNM.Residencias/App.xaml.cs
@@ -0,0 +1,10 @@
+namespace TECNM.Residencias;
+
+using System.Windows;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public sealed partial class App : Application
+{
+}
---
+++ TECNM.Residencias/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+namespace TECNM.Residencias;
+
+using System.Windows;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
